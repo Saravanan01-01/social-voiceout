d7489ddc38bfb875270d22c79620d5a95258f546
saravanan m <saravananmanickam1234@gmail.com>
2021-01-10 14:01:36 +0530

    release 1.0.3

---
+++ vendor/composer/installed.php
@@ -0,0 +1,24 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'dev-master',
+    'version' => 'dev-master',
+    'aliases' => 
+    array (
+    ),
+    'reference' => '3b6ae70d50db1c0a18ffa1196ae0762461f30623',
+    'name' => '__root__',
+  ),
+  'versions' => 
+  array (
+    '__root__' => 
+    array (
+      'pretty_version' => 'dev-master',
+      'version' => 'dev-master',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '3b6ae70d50db1c0a18ffa1196ae0762461f30623',
+    ),
+  ),
+);
